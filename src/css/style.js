99e76e16a7bd3d3f1a9500a841e4adaf7f6febe4
woodlik <sqvoch@gmail.com>
2018-05-06 10:36:49 +0300

    start proj

---
+++ src/css/style.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'view': {
+    'border': [{ 'unit': 'px', 'value': 2 }, { 'unit': 'string', 'value': 'solid' }, { 'unit': 'string', 'value': 'black' }],
+    'display': 'inline-block'
+  },
+  'textView': {
+    'fontSize': [{ 'unit': 'px', 'value': 30 }],
+    'fontFamily': 'monospace',
+    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+    'whiteSpace': 'pre-wrap'
+  },
+  'svg-square': {
+    'fill': 'yellow',
+    'stroke': 'black'
+  },
+  '#speed': {
+    'transform': 'rotate(180deg)'
+  }
+});
